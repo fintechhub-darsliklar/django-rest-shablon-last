5106279c030ba8bb2aab916af0557e945e3a5718
Ziyodullo <ziyodullodev@gmail.com>
2026-05-24 11:47:19 +0500

    add: register UserOTPVerifications, UserOTPIDVerifications, ChangePasswordLogs, and ChangeEmailLogs admin models with detailed configurations

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -26,7 +26,136 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 
-admin.site.register(UserOTPVerifications)
-admin.site.register(UserOTPIDVerifications)
-admin.site.register(ChangePasswordLogs)
-admin.site.register(ChangeEmailLogs)
+@admin.register(UserOTPVerifications)
+class UserOTPVerificationsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "code",
+        "attapts",
+        "resend_attapts",
+        "for_forget_password",
+        "for_forget_password_verified",
+        "expired_at",
+        "created_at",
+    )
+
+    list_filter = (
+        "for_forget_password",
+        "for_forget_password_verified",
+        "created_at",
+        "expired_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "code",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
+
+
+@admin.register(UserOTPIDVerifications)
+class UserOTPIDVerificationsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "code",
+        "attapts",
+        "expired_at",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "expired_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "code",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
+
+
+@admin.register(ChangePasswordLogs)
+class ChangePasswordLogsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "attapts",
+        "is_changed",
+        "expired_at",
+        "error_expired_at",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_changed",
+        "created_at",
+        "expired_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
+
+    # passwordlarni admin panelda edit qilib yubormaslik uchun
+    exclude = (
+        "old_password",
+        "new_password",
+    )
+
+
+@admin.register(ChangeEmailLogs)
+class ChangeEmailLogsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "old_email",
+        "new_email",
+        "code",
+        "attapts",
+        "resend_attapts",
+        "is_changed",
+        "expired_at",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_changed",
+        "created_at",
+        "expired_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "old_email",
+        "new_email",
+        "code",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
